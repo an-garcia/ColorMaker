364ffb4e904837a2bac2a4273d32e88ee9c4ec2d
Angel Newton <xanewton@gmail.com>
2017-10-29 22:02:05 -0400

    Add initial selection colors

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -8,16 +8,46 @@
 
 import UIKit
 
+// MARK: - ViewController: UIViewController
+
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISwitch!
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set colorView on launch
+        changeColorComponent()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
